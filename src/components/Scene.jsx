a8ec73c8275675f5f165f7a80b6c6dbb4ba98f91
Manav <manav@example.com>
2026-04-23 23:50:43 +0530

    fix: make 3d background glow brighter and objects more visible

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -17,18 +17,19 @@ function FloatingAgarbatti({ position, rotation, scale = 1 }) {
       <mesh ref={meshRef} rotation={rotation} scale={scale}>
         {/* Stick base (bamboo) */}
         <cylinderGeometry args={[0.02, 0.02, 3, 16]} />
-        <meshStandardMaterial color="#8b4513" />
+        <meshStandardMaterial color="#8b4513" roughness={0.7} />
         
         {/* Incense part */}
         <mesh position={[0, -0.5, 0]}>
           <cylinderGeometry args={[0.08, 0.08, 2, 16]} />
-          <meshStandardMaterial color="#4a4a4a" roughness={0.9} />
+          <meshStandardMaterial color="#4a4a4a" roughness={0.8} />
         </mesh>
         
         {/* Glowing Tip */}
         <mesh position={[0, 1.5, 0]}>
-          <sphereGeometry args={[0.04, 16, 16]} />
-          <meshStandardMaterial color="#ff4500" emissive="#ff4500" emissiveIntensity={2} />
+          <sphereGeometry args={[0.06, 16, 16]} />
+          <meshStandardMaterial color="#ff4500" emissive="#ff6600" emissiveIntensity={8} />
+          <pointLight distance={4} intensity={2} color="#ff4500" />
         </mesh>
       </mesh>
     </Float>
@@ -50,12 +51,13 @@ function FloatingDhoopCone({ position, rotation, scale = 1 }) {
       <mesh ref={meshRef} rotation={rotation} scale={scale}>
         {/* Dhoop Cone body */}
         <coneGeometry args={[0.3, 1, 16]} />
-        <meshStandardMaterial color="#4a3b32" roughness={0.9} />
+        <meshStandardMaterial color="#5a4538" roughness={0.8} />
         
         {/* Glowing Tip */}
         <mesh position={[0, 0.5, 0]}>
-          <sphereGeometry args={[0.06, 16, 16]} />
-          <meshStandardMaterial color="#ff4500" emissive="#ff4500" emissiveIntensity={2} />
+          <sphereGeometry args={[0.08, 16, 16]} />
+          <meshStandardMaterial color="#ff4500" emissive="#ff6600" emissiveIntensity={8} />
+          <pointLight distance={4} intensity={2} color="#ff4500" />
         </mesh>
       </mesh>
     </Float>
@@ -68,28 +70,28 @@ export default function Scene() {
       <Canvas camera={{ position: [0, 0, 10], fov: 45 }}>
         <color attach="background" args={['#030303']} />
         
-        {/* Lighting */}
-        <ambientLight intensity={0.2} />
-        <directionalLight position={[10, 10, 5]} intensity={1} color="#d4af37" />
-        <pointLight position={[-10, -10, -10]} intensity={0.5} color="#8b4513" />
+        {/* Lighting (increased to make objects more visible) */}
+        <ambientLight intensity={0.6} />
+        <directionalLight position={[10, 10, 5]} intensity={1.5} color="#d4af37" />
+        <pointLight position={[-10, -10, -10]} intensity={1} color="#8b4513" />
 
         {/* Particles representing smoke/scent */}
-        <Sparkles count={50} scale={12} size={2} speed={0.4} opacity={0.3} color="#d4af37" />
+        <Sparkles count={80} scale={12} size={3} speed={0.4} opacity={0.5} color="#d4af37" />
         
         {/* Distant stars/dust */}
         <Stars radius={100} depth={50} count={500} factor={4} saturation={0} fade speed={1} />
 
         {/* Small floating Agarbattis in the background */}
-        <FloatingAgarbatti position={[-6, 3, -8]} rotation={[0, 0, 0.5]} scale={0.6} />
-        <FloatingAgarbatti position={[5, 4, -10]} rotation={[0.2, 0, -0.6]} scale={0.7} />
-        <FloatingAgarbatti position={[-4, -4, -6]} rotation={[-0.2, 0, 0.8]} scale={0.5} />
-        <FloatingAgarbatti position={[6, -3, -9]} rotation={[0.4, 0, -0.4]} scale={0.8} />
+        <FloatingAgarbatti position={[-6, 3, -8]} rotation={[0, 0, 0.5]} scale={0.7} />
+        <FloatingAgarbatti position={[5, 4, -10]} rotation={[0.2, 0, -0.6]} scale={0.8} />
+        <FloatingAgarbatti position={[-4, -4, -6]} rotation={[-0.2, 0, 0.8]} scale={0.6} />
+        <FloatingAgarbatti position={[6, -3, -9]} rotation={[0.4, 0, -0.4]} scale={0.9} />
 
         {/* Small floating Dhoop Cones in the background */}
-        <FloatingDhoopCone position={[-5, 0, -7]} rotation={[0.3, 0.2, 0]} scale={0.8} />
-        <FloatingDhoopCone position={[4, 1, -6]} rotation={[-0.4, -0.1, 0]} scale={0.9} />
-        <FloatingDhoopCone position={[2, 5, -8]} rotation={[0.2, 0.5, 0.1]} scale={0.6} />
-        <FloatingDhoopCone position={[-3, -5, -9]} rotation={[-0.3, 0.1, -0.2]} scale={0.7} />
+        <FloatingDhoopCone position={[-5, 0, -7]} rotation={[0.3, 0.2, 0]} scale={0.9} />
+        <FloatingDhoopCone position={[4, 1, -6]} rotation={[-0.4, -0.1, 0]} scale={1} />
+        <FloatingDhoopCone position={[2, 5, -8]} rotation={[0.2, 0.5, 0.1]} scale={0.7} />
+        <FloatingDhoopCone position={[-3, -5, -9]} rotation={[-0.3, 0.1, -0.2]} scale={0.8} />
 
         {/* Larger Foreground Elements */}
         <FloatingAgarbatti position={[-3, 0, -2]} rotation={[0, 0, 0.2]} scale={1.2} />
